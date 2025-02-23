de65ad8cd8507dc1fb87b1e185f98629143986d6
JosielOliveira <joliviera.prof@gmail.com>
2025-02-23 17:24:57 +0100

    creo espacios

---
+++ espacios.js
@@ -0,0 +1,8 @@
+export const espacios = [
+    { id: "sala-danza", nombre: "Sala de Danza" },
+    { id: "sala-juegos", nombre: "Sala de Juegos" },
+    { id: "cama-elastica", nombre: "Cama Elástica" },
+    { id: "escalada", nombre: "Escalada" },
+    { id: "sala-musical", nombre: "Sala Musical" },
+  ];
+  
