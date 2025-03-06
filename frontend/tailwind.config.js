9550461b4e03a19c23208b679fc0b94ef7ed1ea9
JosielOliveira <joliviera.prof@gmail.com>
2025-03-06 23:54:57 +0100

    frontend refresh

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Asegúrate de incluir las extensiones de archivos que usas en tu proyecto
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
