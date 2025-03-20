2300ee7db9912ff54b00dbfbe0dacc28ac06c86f
JosielOliveira <joliviera.prof@gmail.com>
2025-03-20 22:04:55 +0100

    refresh

--- backend/data/espacios.js
+++
@@ -1,9 +0,0 @@
-const espacios = [
-  { id: "sala-danza", nombre: "Sala de Danza" },
-  { id: "sala-juegos", nombre: "Sala de Juegos" },
-  { id: "cama-elastica", nombre: "Cama Elástica" },
-  { id: "escalada", nombre: "Escalada" },
-  { id: "sala-musical", nombre: "Sala Musical" }
-];
-
-module.exports = { espacios }; 
